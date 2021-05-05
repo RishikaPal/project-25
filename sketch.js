21409327ab7fa165a482ec2f025e7a46e2436328
RishikaPal <80198528+RishikaPal@users.noreply.github.com>
2021-05-05 20:04:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var basket, paperObject;
+var  groundObject
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+function setup() {
+  createCanvas(1600, 700); 
+    
+    engine = Engine.create();
+    world = engine.world;
+
+      groundObject=new ground(width/2,670,width,20);
+
+    paperObject=new paper(200,450,70);
+
+    Engine.run(engine);
+
+    basket=new dustbin(1200,650);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("pink");
+  
+  drawSprites();
+ 
+  groundObject.display();
+
+  paperObject.display();
+
+  basket.display();
+  
+}
+
+
+ function keyPressed()
+   {
+     if (keyCode === UP_ARROW) { 
+       Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85}); 
+     }
+  }
+
+
+
